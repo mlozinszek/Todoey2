ff676451844f85ef0852a37cdd3a7ac9ac2e4713
Michal <m.lozinszek@gmail.com>
2020-05-07 13:56:42 +0200

    Initial Commit

---
+++ Todoey/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Michał Lozinszek on 07/05/2020.
+//  Copyright © 2020 Michał Lozinszek. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
